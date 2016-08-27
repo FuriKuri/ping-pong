4259e50e5208ec7929e478388aaff9ae6b6ab878
Theo Pack Test <tf.pack@gmail.com>
2016-08-28 00:03:08 +0200

    Add ping pong logic

--- ping-pong.go
+++ ping-pong.go
@@ -2,14 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	defaultHitChance = 50
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, hitOrMiss(hitChange()))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func hitChange() int {
+	argsWithoutProg := os.Args[1:]
+	hitChanceIndex := -1
+	for index, element := range argsWithoutProg {
+		if element == "--hit-chance" {
+			hitChanceIndex = index + 1
+		}
+	}
+	if hitChanceIndex != -1 {
+		i, _ := strconv.Atoi(argsWithoutProg[hitChanceIndex])
+		return i
+	}
+	return defaultHitChance
+}
+
+func hitOrMiss(hitChange int) string {
+	result := rand.Intn(100) + 1
+	if result > hitChange {
+		return "MISS"
+	}
+	return "HIT"
 }
