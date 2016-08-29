e45a2542cedc2e2f96c914c80ddec1aae9c458be
Theo Pack Test <tf.pack@gmail.com>
2016-08-29 21:58:30 +0200

    Split client into own file

---
+++ client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	defaultHitChance = "50"
+)
+
+func ClientHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, hitOrMiss(hitChange()))
+}
+
+func hitChange() int {
+	i, _ := strconv.Atoi(getArgParameter("hit-chance", defaultHitChance))
+	return i
+}
+
+func getArgParameter(name string, defaultValue string) string {
+	argsWithoutProg := os.Args[1:]
+	for index, element := range argsWithoutProg {
+		if element == "--"+name {
+			return argsWithoutProg[index+1]
+		}
+	}
+	return defaultValue
+}
+
+func hitOrMiss(hitChange int) string {
+	result := rand.Intn(100) + 1
+	if result > hitChange {
+		return "MISS"
+	}
+	return "HIT"
+}
--- ping-pong.go
+++ ping-pong.go
@@ -1,45 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
 	"net/http"
-	"os"
-	"strconv"
 )
 
 const (
-	defaultHitChance = "50"
+	defaultMode = "client"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, hitOrMiss(hitChange()))
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", ClientHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func hitChange() int {
-	i, _ := strconv.Atoi(getArgParameter("hit-chance", defaultHitChance))
-	return i
-}
-
-func getArgParameter(name string, defaultValue string) string {
-	argsWithoutProg := os.Args[1:]
-	for index, element := range argsWithoutProg {
-		if element == "--"+name {
-			return argsWithoutProg[index+1]
-		}
-	}
-	return defaultValue
-}
-
-func hitOrMiss(hitChange int) string {
-	result := rand.Intn(100) + 1
-	if result > hitChange {
-		return "MISS"
-	}
-	return "HIT"
+func mode() string {
+	return getArgParameter("mode", defaultMode)
 }
