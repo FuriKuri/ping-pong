f6afbd555430d8e483080f67a9b174f0c6bb5924
Theo Pack Test <tf.pack@gmail.com>
2016-08-30 15:14:02 +0200

    Add first ping pong

--- server.go
+++ server.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+)
+
+const (
+	pingEndpoint = "http://ping:8080/"
+	pongEndpoint = "http://pong:8080/"
 )
 
 // ServerHandler return the HttpFunction for server mode.
 func ServerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hitOrMiss(hitChange()")
+	log.Println("Start game")
+	fmt.Fprintf(w, doRequest(pingEndpoint))
+	fmt.Fprintf(w, doRequest(pongEndpoint))
+}
+
+func doRequest(endpoint string) string {
+	resp, err := http.Get(endpoint)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	s := string(body[:])
+	return s
 }
